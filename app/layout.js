f221a6d29d58f491ab426c07afe6acd344547eaf
Stephen-sudoa <nanagyasi180@gmail.com>
2025-07-10 13:10:41 +0100

    Wrap Header and children in Suspense for improved loading state handling

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,10 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Header />
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Header />
+            {children}
+          </Suspense>
         </body>
       </html>
     </ClerkProvider>
